3064664bfeb3d7d912bb99c8ceb589047271acd0
rajput1813 <rahulrajput1318@gmail.com>
2021-07-20 11:25:30 +0530

    futurehandlingrepeated

--- src/main/scala/MongoDbExample.scala
+++ src/main/scala/MongoDbExample.scala
@@ -1,31 +1,37 @@
 import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
-
 import org.mongodb.scala.{MongoClient, MongoCollection, MongoDatabase}
 import org.bson.codecs.configuration.CodecRegistries._
 import org.mongodb.scala.MongoClient.DEFAULT_CODEC_REGISTRY
 
 import scala.language.postfixOps
 import org.mongodb.scala.bson.codecs.Macros._
+import org.mongodb.scala.model.Filters.equal
+import org.mongodb.scala.model.Updates.set
 import org.mongodb.scala.{MongoClient, MongoCollection, MongoDatabase, Observable, Observer}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
 object MongoDbExample {
   def main(args: Array[String]): Unit = {
-    val myParcel = Student(
-      sender = Address("nepal", "Birgung", "Berlin"),
-      name = "rahulsingh",
-      rollno = 17115064,
-      branch = "CSE",
+    val people: Seq[Student]  = Seq(Student(sender = Address("nepal", "Birgung", "Berlin"), name = "rahulsingh", rollno = 17115064, branch = "CSE",
+      clgname = "Nitrr"),
+     Student(sender = Address("a", "B", "c"), name = "a", rollno = 1, branch = "CSE",
+      clgname = "Nitrr"),
+     Student(sender = Address("r", "g", "Bb"), name = "singh", rollno = 12, branch = "ece",
       clgname = "Nitrr")
+    )
 
     val codecRegistry = fromRegistries(fromProviders(classOf[Student],classOf[Address]), DEFAULT_CODEC_REGISTRY)
     val client: MongoClient = MongoClient()
     val database: MongoDatabase = client.getDatabase("mydb1").withCodecRegistry(codecRegistry)
     val students: MongoCollection[Student] = database.getCollection("student")
-    Await.result(students.insertOne(myParcel).toFuture, 10 seconds)
-    students.find().foreach(println)
-    client.listDatabaseNames().foreach(println)
+   // Await.result(students.insertMany(people).toFuture(),10 seconds)
+   val allStudent= Await.result(database.getCollection("student").find().first().toFuture(),10 seconds)
+    allStudent.foreach(println)
+  //  Await.result(students.updateOne(equal("name", "singh"), set("rollno", 14)).toFuture(),10 seconds)
+    Await.result(students. deleteOne(equal("name", "a")).toFuture(),10 seconds)
+
+
   }
 }
 case class Student(sender: Address,
--- src/main/scala/futurehandling/FuturesOptionsHandling.scala
+++ src/main/scala/futurehandling/FuturesOptionsHandling.scala
@@ -8,44 +8,44 @@ object FuturesOptionsHandling extends App {
   //Hint - Try map/for-comprehension on Futures.
   //Use .copy() to make copies of immutable objects in which you want to update data
   //object FuturesOptionsHandling extends App {
-      case class Job(jobId: String, title: String, clicks: Option[Int] = None, applies: Option[Int] = None)
-      case class ClicksStat(jobId: String, clicks: Int)
-      case class AppliesStat(jobId: String, applies: Int)
-      val jobs = Future.successful(List(Job("job1", "title1"), Job("job2", "title1"), Job("job3", "title3")))
-      val clicks = Future.successful(List(ClicksStat("job2", 50)))
-      val applies = List(AppliesStat("job3", 150))
+  case class Job(jobId: String, title: String, clicks: Option[Int] = None, applies: Option[Int] = None)
+  case class ClicksStat(jobId: String, clicks: Int)
+  case class AppliesStat(jobId: String, applies: Int)
+  val jobs = Future.successful(List(Job("job1", "title1"), Job("job2", "title1"), Job("job3", "title3")))
+  val clicks = Future.successful(List(ClicksStat("job2", 50)))
+  val applies = List(AppliesStat("job3", 150))
   val updatedJobs = Future.successful( List(Job("job1", "title1"), Job("job2", "title1", Some(50), None), Job("job3", "title3", None, Some(150))))
 
   // If stats are not present clicks/Applies should be None
 
-      val jobsEnrichedWithStats = Future{
-        val list1:Future[List[Job]] =updatedJobs.map(x=> x.filter(x=> x.applies!=None|| x.clicks!=None ))
-       //println( Await.result(jobs.map(x=> x.filter(x=> x.clicks!=None )),6.seconds))
-        //Await.result(list1,5.seconds)
-        //println(list1);
+  val jobsEnrichedWithStats = Future{
+    val list1:Future[List[Job]] =updatedJobs.map(x=> x.filter(x=> x.applies!=None|| x.clicks!=None ))
+    //println( Await.result(jobs.map(x=> x.filter(x=> x.clicks!=None )),6.seconds))
+    //Await.result(list1,5.seconds)
+    //println(list1);
 
 
-          list1;
+    list1;
 
-      }
+  }
   // It should contain => Future(List(Job("job1", "title1"), Job("job2", "title1", Some(50), None), Job("job3", "title3", None, Some(150))))
   Await.result(jobsEnrichedWithStats,5.seconds)
-    println(jobsEnrichedWithStats)
-     val jobsWithClicksNotNone =  Future{
-       val list1:Future[List[Job]] =updatedJobs.map(x=> x.filter(x=> x.clicks!=None ))
-       list1
-     }
+  println(jobsEnrichedWithStats)
+  val jobsWithClicksNotNone =  Future{
+    val list1:Future[List[Job]] =updatedJobs.map(x=> x.filter(x=> x.clicks!=None ))
+    list1
+  }
   Await.result(jobsWithClicksNotNone,5.seconds)
-     println(jobsWithClicksNotNone)
-     val jobsWithAppliesNotNone =  Future{
-        val list1:Future[List[Job]] =updatedJobs.map(x=> x.filter(x=> x.applies!=None ))
-        list1
-       }
-    Await.result(jobsWithAppliesNotNone,5.seconds)
-      println(jobsWithAppliesNotNone)
-//      //  title -> List[Job]
-      val jobsGroupedByTitle  =  Future{
-        val myMap :Future[Map[String ,List[Job] ]] = updatedJobs.map(x=> x.groupBy(x=>x.title))
+  println(jobsWithClicksNotNone)
+  val jobsWithAppliesNotNone =  Future{
+    val list1:Future[List[Job]] =updatedJobs.map(x=> x.filter(x=> x.applies!=None ))
+    list1
+  }
+  Await.result(jobsWithAppliesNotNone,5.seconds)
+  println(jobsWithAppliesNotNone)
+  //      //  title -> List[Job]
+  val jobsGroupedByTitle  =  Future{
+    val myMap :Future[Map[String ,List[Job] ]] = updatedJobs.map(x=> x.groupBy(x=>x.title))
 
     myMap
   }
@@ -53,58 +53,58 @@ object FuturesOptionsHandling extends App {
   //Await.result(jobsGroupedByTitle,200.seconds)
 
   println(jobsGroupedByTitle)
-//      // Should return Map(title -> (sumClicks, sumApplies)). if clicks/applies is None, set its value as 0
-//     val statsPerTitle =   Future{
-//       val myMap :Future[Map[String ,List[Job] ]] = updatedJobs.map(x=> x.groupBy(x=>x.title))
-//       val myMap1 :Future[Map[String ,Int]] = myMap.map(x=> x.values.foreach(y=> y.foreach(z=> z.clicks.getOrElse(0)))).to
-//
-//
-//       myMap
-//     }
+  //      // Should return Map(title -> (sumClicks, sumApplies)). if clicks/applies is None, set its value as 0
+  //     val statsPerTitle =   Future{
+  //       val myMap :Future[Map[String ,List[Job] ]] = updatedJobs.map(x=> x.groupBy(x=>x.title))
+  //       val myMap1 :Future[Map[String ,Int]] = myMap.map(x=> x.values.foreach(y=> y.foreach(z=> z.clicks.getOrElse(0)))).to
+  //
+  //
+  //       myMap
+  //     }
   //}
 
 
-//      case class Job(jobId: String, title: String, clicks: Option[Int] = None, applies: Option[Int] = None)
-//      case class ClicksStat(jobId: String, clicks: Int)
-//      case class AppliesStat(jobId: String, applies: Int)
-//      val jobs = Future.successful(List(Job("job1", "title1"), Job("job2", "title1"), Job("job3", "title3")))
-//      val clicks = Future.successful(List(ClicksStat("job2",50)))
-//      val applies = List(AppliesStat("job3", 150))
-//      val jobsEnrichedWithStats = Future{
-//        var  list :List[Job]= List(Job("job1", "title1"), Job("job2", "title1", Some(50), None), Job("job3", "title3", None, Some(150)))
-//                 list
-//
-//      }
-//  println(jobsEnrichedWithStats)
-//        val jobsWithClicksNotNone = Future{
-//          var  list1 :List[Job]= List(Job("job1", "title1"), Job("job2", "title1", Some(50), None), Job("job3", "title3", None, Some(150)))
-//          var  list2 :List[Job] = for(x <- list1 if(x.clicks!=None)) yield x
-//          list2
-//        }
-//   Thread. sleep(5000)
-//     println(jobsWithClicksNotNone)
-////        val jobsWithAppliesNotNone =
-//val jobsWithAppliesNotNone = Future{
-//  var  list1 :List[Job]= List(Job("job1", "title1"), Job("job2", "title1", Some(50), None), Job("job3", "title3", None, Some(150)))
-//  var  list2 :List[Job] = for(x <- list1 if(x.applies!=None)) yield x
-//  list2
-//}
-//  Thread. sleep(5000)
-//  println(jobsWithAppliesNotNone)
-//
-//  //  title -> List[Job]
-//  //    val jobsGroupedByTitle = ???
-//
-//  val  jobsGroupedByTitle =Future{
-//    var  list1 :List[Job]= List(Job("job1", "title1"), Job("job2", "title1", Some(50), None), Job("job3", "title3", None, Some(150)))
-//    var myMap :Map[String ,List[Job] ] = list1.groupBy(x=> x.title)
-//    myMap
-//  }
-//  Thread. sleep(5000)
-//  println(jobsGroupedByTitle)
-//  val statsPerTitle = Future{
-//    var  list1 :List[Job]= List(Job("job1", "title1"), Job("job2", "title1", Some(50), None), Job("job3", "title3", None, Some(150)))
-//
-//
-//  }
+  //      case class Job(jobId: String, title: String, clicks: Option[Int] = None, applies: Option[Int] = None)
+  //      case class ClicksStat(jobId: String, clicks: Int)
+  //      case class AppliesStat(jobId: String, applies: Int)
+  //      val jobs = Future.successful(List(Job("job1", "title1"), Job("job2", "title1"), Job("job3", "title3")))
+  //      val clicks = Future.successful(List(ClicksStat("job2",50)))
+  //      val applies = List(AppliesStat("job3", 150))
+  //      val jobsEnrichedWithStats = Future{
+  //        var  list :List[Job]= List(Job("job1", "title1"), Job("job2", "title1", Some(50), None), Job("job3", "title3", None, Some(150)))
+  //                 list
+  //
+  //      }
+  //  println(jobsEnrichedWithStats)
+  //        val jobsWithClicksNotNone = Future{
+  //          var  list1 :List[Job]= List(Job("job1", "title1"), Job("job2", "title1", Some(50), None), Job("job3", "title3", None, Some(150)))
+  //          var  list2 :List[Job] = for(x <- list1 if(x.clicks!=None)) yield x
+  //          list2
+  //        }
+  //   Thread. sleep(5000)
+  //     println(jobsWithClicksNotNone)
+  ////        val jobsWithAppliesNotNone =
+  //val jobsWithAppliesNotNone = Future{
+  //  var  list1 :List[Job]= List(Job("job1", "title1"), Job("job2", "title1", Some(50), None), Job("job3", "title3", None, Some(150)))
+  //  var  list2 :List[Job] = for(x <- list1 if(x.applies!=None)) yield x
+  //  list2
+  //}
+  //  Thread. sleep(5000)
+  //  println(jobsWithAppliesNotNone)
+  //
+  //  //  title -> List[Job]
+  //  //    val jobsGroupedByTitle = ???
+  //
+  //  val  jobsGroupedByTitle =Future{
+  //    var  list1 :List[Job]= List(Job("job1", "title1"), Job("job2", "title1", Some(50), None), Job("job3", "title3", None, Some(150)))
+  //    var myMap :Map[String ,List[Job] ] = list1.groupBy(x=> x.title)
+  //    myMap
+  //  }
+  //  Thread. sleep(5000)
+  //  println(jobsGroupedByTitle)
+  //  val statsPerTitle = Future{
+  //    var  list1 :List[Job]= List(Job("job1", "title1"), Job("job2", "title1", Some(50), None), Job("job3", "title3", None, Some(150)))
+  //
+  //
+  //  }
 }
